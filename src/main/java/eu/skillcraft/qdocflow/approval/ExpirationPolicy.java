b638906fc4a81885b5ac957a87b1434216f4eacf
Łukasz Szydło <szydler@gmail.com>
2019-04-26 08:42:05 +0200

    extract policy to class

--- src/main/java/eu/skillcraft/qdocflow/approval/ExpirationPolicy.java
+++ src/main/java/eu/skillcraft/qdocflow/approval/ExpirationPolicy.java
@@ -6,9 +6,6 @@ import java.time.LocalDate;
 
 interface ExpirationPolicy {
 
-	ExpirationPolicy DEFAULT = () -> LocalDate.now().plusDays(20);
-	ExpirationPolicy FAST_TRACK = () -> LocalDate.now().plusDays(2);
-
 	LocalDate calculate();
 
 	@AllArgsConstructor
@@ -18,10 +15,27 @@ interface ExpirationPolicy {
 
 		ExpirationPolicy create() {
 			if (currentUserPort.isAuditor()) {
-				return FAST_TRACK;
+				return new FastTrackExpirationPolicy();
 			} else {
-				return DEFAULT;
+				return new DefaultExpirationPolicy();
 			}
+		}
+	}
+
+
+	class DefaultExpirationPolicy implements ExpirationPolicy {
+
+		@Override
+		public LocalDate calculate() {
+			return LocalDate.now().plusDays(20);
+		}
+	}
+
+	class FastTrackExpirationPolicy implements ExpirationPolicy {
+
+		@Override
+		public LocalDate calculate() {
+			return LocalDate.now().plusDays(21);
 		}
 	}
 
